6dbd237e3c70a8da1c37eb1e97cf00da7b3c8444
Jack Runburg <jack.runburg@gmail.com>
2021-09-02 12:30:41 -1000

    Fix bug in p2 at large r

--- sommerfeld_dm_wolfram.py
+++ sommerfeld_dm_wolfram.py
@@ -11,13 +11,12 @@ from scipy import integrate
 
 from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wl, wlexpr
-# session = WolframLanguageSession()
+session = WolframLanguageSession()
 
 orig_stdout = sys.stdout
 
 lim = 1100
 PLOT = True
-ANALYTIC = False
 
 def nfw_gamma(r, gamma):
     return r**-gamma * (1 + r)**-(3 - gamma)
@@ -32,46 +31,25 @@ def burkert(r):
 def moore(r):
     return (r**1.4 * (1 + r)**1.4)**-1
 
-# rho_dict = {
-#         'nfw0-6': ['nfw', 0.6, lambda r: nfw_gamma(r, 0.6), 2.66623**-4 * (-1.3656)**2, 1.69103],
-#         'nfw0-7': ['nfw', 0.7, lambda r: nfw_gamma(r, 0.7), 2.03277**-4 * (-1.0006)**2, 1.5188],
-#         'nfw0-8': ['nfw', 0.8, lambda r: nfw_gamma(r, 0.8), 1.56803**-4 * (-0.7275)**2, 1.38725],
-#         'nfw0-9': ['nfw', 0.9, lambda r: nfw_gamma(r, 0.9), 1.21591**-4 * (-0.5230)**2, 1.28305],
-#         'nfw1-0': ['nfw', 1.0, lambda r: nfw_gamma(r, 1.0), 0.94281**-4 * (-0.3702)**2, 1.19814],
-#         'nfw1-1': ['nfw', 1.1, lambda r: nfw_gamma(r, 1.1), 0.72736**-4 * (-0.2569)**2, 1.1274],
-#         'nfw1-2': ['nfw', 1.2, lambda r: nfw_gamma(r, 1.2), 0.55536**-4 * (-0.1736)**2, 1.06738],
-#         'nfw1-3': ['nfw', 1.3, lambda r: nfw_gamma(r, 1.3), 0.41702**-4 * (-0.1132)**2, 1.01569],
-#         'nfw1-4': ['nfw', 1.4, lambda r: nfw_gamma(r, 1.4), 0.30540**-4 * (-0.0704)**2, 0.970611],
-#         # 'nfw1-27': ['nfw', 1.27, lambda r: nfw_gamma(r, 1.27), 0.455443**-4 * (-0.1292)**2, 1.03044],
-#         'nfw1-25': ['nfw', 1.25, lambda r: nfw_gamma(r, 1.25), 0.482469**-4 * (-0.1209)**2, 1.04061],
-#         'einasto0-13': ['einasto', 0.13, lambda r: einasto(r, 0.13), 0, 0],
-#         'einasto0-16': ['einasto', 0.16, lambda r: einasto(r, 0.16), 0, 0],
-#         'einasto0-17': ['einasto', 0.17, lambda r: einasto(r, 0.17), 0, 0],
-#         'einasto0-20': ['einasto', 0.20, lambda r: einasto(r, 0.20), 0, 0],
-#         'einasto0-24': ['einasto', 0.24, lambda r: einasto(r, 0.24), 0, 0],
-#         'burkert': ['burkert', 0, lambda r: burkert(r), 0, 0],
-#         'moore': ['moore', 0, lambda r: moore(r), 0, 0],
-# }
-
 rho_dict = {
-        'nfw0-6': ['nfw', 0.6, lambda r: nfw_gamma(r, 0.6), 0.0177549, 0.352125, 1.69103],
-        'nfw0-7': ['nfw', 0.7, lambda r: nfw_gamma(r, 0.7), 0.0217055, 0.418731, 1.51880],
-        'nfw0-8': ['nfw', 0.8, lambda r: nfw_gamma(r, 0.8), 0.0265685, 0.480743, 1.38725],
-        'nfw0-9': ['nfw', 0.9, lambda r: nfw_gamma(r, 0.9), 0.0329904, 0.537610, 1.28305],
-        'nfw1-0': ['nfw', 1.0, lambda r: nfw_gamma(r, 1.0), 3 / (16 * np.sqrt(2) * np.pi), 3 * np.pi / 16, 1.19814],
-        'nfw1-1': ['nfw', 1.1, lambda r: nfw_gamma(r, 1.1), 0.0567894, 0.634935, 1.12740],
-        'nfw1-2': ['nfw', 1.2, lambda r: nfw_gamma(r, 1.2), 0.0829223, 0.675237, 1.06738],
-        'nfw1-3': ['nfw', 1.3, lambda r: nfw_gamma(r, 1.3), 0.1381440, 0.675237, 1.01569],
-        'nfw1-4': ['nfw', 1.4, lambda r: nfw_gamma(r, 1.4), 0.2866070, 0.739172, 0.97061],
-        # 'nfw1-27': ['nfw', 1.27, lambda r: nfw_gamma(r, 1.27), 0.455443**-4 * (-0.1292)**2, 1.03044],
-        'nfw1-25': ['nfw', 1.25, lambda r: nfw_gamma(r, 1.25), 0.104831, 0.693298, 1.04061],
-        'einasto0-13': ['einasto', 0.13, lambda r: einasto(r, 0.13), 0, 0, 0],
-        'einasto0-16': ['einasto', 0.16, lambda r: einasto(r, 0.16), 0, 0, 0],
-        'einasto0-17': ['einasto', 0.17, lambda r: einasto(r, 0.17), 0, 0, 0],
-        'einasto0-20': ['einasto', 0.20, lambda r: einasto(r, 0.20), 0, 0, 0],
-        'einasto0-24': ['einasto', 0.24, lambda r: einasto(r, 0.24), 0, 0, 0],
-        'burkert': ['burkert', 0, lambda r: burkert(r), 0, 0, 0],
-        'moore': ['moore', 0, lambda r: moore(r), 0, 0, 0],
+        'nfw0-6': ['nfw', 0.6, lambda r: nfw_gamma(r, 0.6), 2.66623**-4 * (-1.3656)**2, 1.69103],
+        'nfw0-7': ['nfw', 0.7, lambda r: nfw_gamma(r, 0.7), 2.03277**-4 * (-1.0006)**2, 1.5188],
+        'nfw0-8': ['nfw', 0.8, lambda r: nfw_gamma(r, 0.8), 1.56803**-4 * (-0.7275)**2, 1.38725],
+        'nfw0-9': ['nfw', 0.9, lambda r: nfw_gamma(r, 0.9), 1.21591**-4 * (-0.5230)**2, 1.28305],
+        'nfw1-0': ['nfw', 1.0, lambda r: nfw_gamma(r, 1.0), 0.94281**-4 * (-0.3702)**2, 1.19814],
+        'nfw1-1': ['nfw', 1.1, lambda r: nfw_gamma(r, 1.1), 0.72736**-4 * (-0.2569)**2, 1.1274],
+        'nfw1-2': ['nfw', 1.2, lambda r: nfw_gamma(r, 1.2), 0.55536**-4 * (-0.1736)**2, 1.06738],
+        'nfw1-3': ['nfw', 1.3, lambda r: nfw_gamma(r, 1.3), 0.41702**-4 * (-0.1132)**2, 1.01569],
+        'nfw1-4': ['nfw', 1.4, lambda r: nfw_gamma(r, 1.4), 0.30540**-4 * (-0.0704)**2, 0.970611],
+        'nfw1-27': ['nfw', 1.27, lambda r: nfw_gamma(r, 1.27), 0.455443**-4 * (-0.1292)**2, 1.03044],
+        'nfw1-25': ['nfw', 1.25, lambda r: nfw_gamma(r, 1.25), 0.482469**-4 * (-0.1209)**2, 1.04061],
+        'einasto0-13': ['einasto', 0.13, lambda r: einasto(r, 0.13), 0, 0],
+        'einasto0-16': ['einasto', 0.16, lambda r: einasto(r, 0.16), 0, 0],
+        'einasto0-17': ['einasto', 0.17, lambda r: einasto(r, 0.17), 0, 0],
+        'einasto0-20': ['einasto', 0.20, lambda r: einasto(r, 0.20), 0, 0],
+        'einasto0-24': ['einasto', 0.24, lambda r: einasto(r, 0.24), 0, 0],
+        'burkert': ['burkert', 0, lambda r: burkert(r), 0, 0],
+        'moore': ['moore', 0, lambda r: moore(r), 0, 0],
 }
 
 
@@ -82,48 +60,20 @@ for fil in rho_dict.keys():
         pass
     os.chdir(fil)
     f = open('output.txt', 'w')
-    sys.stdout = f
+    # sys.stdout = f
 
     print('\n\n', fil, '\n')
-    # Setting a range of what our r̃ values will be
     r_vals = np.logspace(-5, 2, num=lim)
 
     np.save('./rvals.npy', r_vals)
 
-    # def rho(r):
-    #     return 1 / (r * (1 + r)**2)
-    label, gamma, rho, f0, cn, Ib = rho_dict[fil]
-
-    # k = (rho(r_vals.min()) * r_vals.min()**gamma)**(5/2) * ((3 - gamma) * (2 - gamma))**0.5
-
-    rho0 = rho(r_vals.min()) * r_vals.min()**gamma
-    beta = (gamma - 6) / (2 * (2 - gamma))
-    b = -1 - gamma * 3 / 2
-
-    # if label == 'nfw':
-    #     # f0 = rho0 / (4 * np.sqrt(2) * np.pi) * (rho0 / ((3 - gamma) * (2 - gamma)))**-(beta + 3/2) / session.evaluate(f'Integrate[x^{beta} (x - 1)^0.5, {x, 0, Infinity}]')
-    #     f0 = 0 
-    #     # print(f0)
-    # else:
-    #     f0 = 0
-
-    # if label == 'nfw':
-    #     # cn = session.evaluate()
-    #     cn=0
-    # else:
-    #     cn = 0
+    label, gamma, rho, A, Ib = rho_dict[fil]
+    k = (rho(r_vals.min()) * r_vals.min()**gamma)**(5/2) * ((3 - gamma) * (2 - gamma))**0.5
 
     rho_vals = rho(r_vals)
 
-
-    def phi_y(x):
-        f = lambda y: y**2 * rho(y)
-        return integrate.quad(f, 0, x, points=r_vals[:150], limit=lim//2)[0]
-
-
-    def phi_x(r):
-        f = lambda x: 1 / (x**2 + 1e-100) * phi_y(x)
-        return -integrate.quad(f, r, 0, points=r_vals[:150], limit=lim//2)[0]
+    def phi(r):
+        return 
 
 
     print('computing phi vals')
@@ -137,12 +87,7 @@ for fil in rho_dict.keys():
 
 
     def analytic_phi(r):
-        return rho0 * r**(2 - gamma) / ((3 - gamma) * (2 - gamma))
-
-
-    # if ANALYTIC is True:
-    #     small_r_approx = (r_vals < 0.001)
-    #     phi_vals[small_r_approx] = analytic_phi(r_vals[small_r_approx])
+        return r**(2 - gamma) / ((3 - gamma) * (2 - gamma))
 
 
     if PLOT is True:
@@ -170,13 +115,13 @@ for fil in rho_dict.keys():
     sec_derv = np.gradient(first_derv, phi_vals)
 
     # making the second derivative a function
-    sec_derv_func = i1d(phi_vals[5:-5], sec_derv[5:-5], fill_value='extrapolate', bounds_error=False)
+    sec_derv_func = i1d(phi_vals, sec_derv, fill_value='extrapolate', bounds_error=False)
 
     print('computing dm phase space distribution')
     # plot with new phi values
 
     def f_analytic(E, gamma=gamma):
-        return f0 * E**beta
+        return 3 / (16 * np.sqrt(2) * np.pi) * E **((gamma - 6) / (2 * (2 - gamma)))
 
     def f(E):
         return integrate.quad(lambda x: 1 / (np.sqrt(8) * np.pi**2) * sec_derv_func(x) / np.sqrt(x * 1.00000000001 - E), E, phi_vals.max(), points=phi_vals, limit=lim+1)[0]
@@ -285,9 +230,9 @@ for fil in rho_dict.keys():
     # vvals = np.linspace(0, np.sqrt(-2*phi_vals[0]), num=lim)
     vvals = np.logspace(-8, np.log10(np.sqrt(2)), num=lim+1)
 
-    fvals = fvals[5:-5]
-    phi_vals = phi_vals[5:-5]
-    r_vals = r_vals[5:-5]
+    fvals = fvals
+    phi_vals = phi_vals
+    r_vals = r_vals
 
     ######
     #frv = np.zeros((len(r_vals), len(vvals)))
@@ -307,20 +252,19 @@ for fil in rho_dict.keys():
 
     #integrand = frv[:, :, np.newaxis] * frv[:, np.newaxis, :] * vvals[np.newaxis, :, np.newaxis]**2 * vvals[np.newaxis, np.newaxis, :] * np.tri(len(vvals)).T[np.newaxis, :, :]
     #p2_som = integrate.simps(integrate.simps(integrand, vvals, axis=2), vvals, axis=1)
-
-    ## print(p2_som)
-
+    #print(p2_som)
     #np.save('p2_sommerfeld.npy', p2_som) # save
     #####
 
     p2_som = np.load('p2_sommerfeld.npy') # save
-    p2_som[p2_som < 1e-6] = 0
+    # p2_som[p2_som < 1e-6] = 0
     print(np.sum(np.isnan(p2_som)), 'nan vals in p2som')
 
 
     def p2_som_analytic(r):
-        return cn * r ** b / (32 * np.pi**2)
+        return A * k**2 * r ** (-1 + gamma * -3/2) / (32 * np.pi**2)
 
+    print(np.sqrt(p2_som_analytic(1)))
 
     if PLOT is True:
         fig, (ax, ax2) = plt.subplots(nrows=2, sharex=True)
@@ -337,6 +281,7 @@ for fil in rho_dict.keys():
         ax.set_xlim(right=1e2)
 
         if label == 'nfw':
+            print(p2_som[0], p2_som_analytic(r_vals.min()), p2_som[0] / p2_som_analytic(r_vals.min()))
             ax2.plot(r_vals, (p2_som - p2_som_analytic(r_vals))/p2_som_analytic(r_vals), label='analytic')
             ax2.set_ylabel('percent residual')
             ax2.set_ylim(bottom=-.1, top=.1)
@@ -373,11 +318,6 @@ for fil in rho_dict.keys():
     theta_vals = np.load('theta_vals.npy')
     J_som = np.load('J_som_vals.npy') # save
 
-
-    def jsom_analytic(theta):
-        return cn * Ib * theta_vals**(1 + b)
-
-
     if PLOT is True:
         with np.load("../df_nfw_h_values.txt") as infile:
             hsomn = infile['hsom']
@@ -388,9 +328,9 @@ for fil in rho_dict.keys():
         ax.plot(radius, hsomn, label='old cod')
 
         if label == 'nfw':
-            ind = 50
-            # print(p2_som[ind] / p2_som_analytic(r_vals[ind]), J_som[ind] / jsom_analytic(theta_vals[ind]))
-            ax.plot(theta_vals, jsom_analytic(theta_vals), label='mathematica', ls='--')
+            b = -1 + gamma * -3 / 2
+            jsom_analytic = A * k**2 * Ib * theta_vals**(b + 1)
+            ax.plot(theta_vals, jsom_analytic, label='mathematica', ls='--')
         ax.set_xscale('log')
         ax.set_yscale('log')
         ax.set_xlabel('theta')
@@ -400,7 +340,7 @@ for fil in rho_dict.keys():
         ax.legend() 
 
         if label == 'nfw':
-            ax2.plot(theta_vals, (J_som - jsom_analytic(theta_vals)) / jsom_analytic(theta_vals), label='analytic')
+            ax2.plot(theta_vals, (J_som - jsom_analytic) / jsom_analytic, label='analytic')
             # ax2.plot(theta_vals, 1 / J_som * jsom_analytic)
             ax2.set_xlabel('theta')
             ax2.set_ylabel('percent residual')
@@ -408,9 +348,9 @@ for fil in rho_dict.keys():
         fig.savefig('./jsom.pdf')
 
     # print(J_som)
-    equation_8_som = integrate.simps(np.nan_to_num(J_som) * theta_vals, theta_vals)
+    equation_8_som = np.trapz(np.nan_to_num(J_som) * theta_vals, theta_vals)
     print('total j factor', equation_8_som)   #eq 8 1.02
-    equation_10_som = integrate.simps(np.nan_to_num(J_som) * theta_vals**2, theta_vals) / equation_8_som
+    equation_10_som = np.trapz(np.nan_to_num(J_som) * theta_vals**2, theta_vals) / equation_8_som
     print('angular spread', equation_10_som)  #eq 10 match the paper 0.27
 
     # equation_8_som = np.trapz(np.nan_to_num(hsomn) * radius, radius)
